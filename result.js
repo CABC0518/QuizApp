6ba0b6d65b095b8435551fd82832668d8924278b
CABC0518 <takumi@aizawa1997@gmail.com>
2020-02-18 18:15:04 +0200

    deleted unnecessary files

--- result.js
+++
@@ -1,6 +0,0 @@
-let score = sessionStorage.getItem("score");
-document.getElementById("scoreboard").innerHTML = "<h2>You got " + score + " answers correct out of 10 questions</h2>";
-
-function GoToHome(){
-    window.location.assign("index.html");
-}
