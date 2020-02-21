cade670debf529dbb5bfb3a922c7afa6d957da37
takumi.aizawa1997@gmail.com <takumi.aizawa1997@gmail.com>
2020-02-21 22:26:10 +0200

    result page transition added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-function getQuestion(){
+function getQuestion() {
   let randomInt = []
   let choices = []
   let answer_word;
@@ -17,21 +15,21 @@ function getQuestion(){
   for (var i = 0; i < randomInt.length; i++) {
     choices.push(words_list[randomInt[i]])
   }
-  answer_word = choices[getRandomInt(0,4)]
+  answer_word = choices[getRandomInt(0, 4)]
   return [choices, answer_word]
 }
 
-function refreshQuiz(){
+function refreshQuiz() {
   let question = document.getElementById("question_word")
   question.textContent = ""
   // refresh choices
   els = document.getElementsByClassName("choices")
-  for(el of els){
+  for (el of els) {
     el.textContent = "";
   }
 }
 
-function makeQuestion(){
+function makeQuestion() {
   refreshQuiz()
   let choices_answer_arr = getQuestion()
   console.log(choices_answer_arr)
@@ -52,7 +50,7 @@ let quiz_count = 0;
 let counter = 0;
 document.getElementById('counter').innerHTML = counter + " out of " + quiz_count + " questions are correct"
 
-document.getElementById('start_btn').addEventListener('click', ()=>{
+document.getElementById('start_btn').addEventListener('click', () => {
   document.getElementById('start_box').style.display = "none";
   document.getElementById('quiz_box').style.display = "block";
   makeQuestion()
@@ -61,38 +59,39 @@ document.getElementById('start_btn').addEventListener('click', ()=>{
 
 let choices_cls = document.getElementsByClassName("choices");
 console.log(choices_cls)
-for(let i = 0; i < choices_cls.length; i++){
-  choices_cls[i].addEventListener("click", ()=>{
-      quiz_count++
-      console.log(quiz_count)
-      let answer_word = localStorage.getItem("answer_word");
-      msgSetNone()
-      // when asnwer is correct
-      if(choices_cls[i].textContent == answer_word){
-        console.log("User Input: ",choices_cls[i].textContent)
-        document.getElementById('correct').style.display = "block";
-        counter++
-        console.log(counter)
-        console.log("correct!")
+for (let i = 0; i < choices_cls.length; i++) {
+  choices_cls[i].addEventListener("click", () => {
+    quiz_count++
+    console.log(quiz_count)
+    let answer_word = localStorage.getItem("answer_word");
+    msgSetNone()
+    // when asnwer is correct
+    if (choices_cls[i].textContent == answer_word) {
+      console.log("User Input: ", choices_cls[i].textContent)
+      document.getElementById('correct').style.display = "block";
+      counter++
+      console.log(counter)
+      console.log("correct!")
       // when asnwer is incorrect
-      }else{
-        console.log("User Input: ",choices_cls[i].textContent)
-        document.getElementById('incorrect').style.display = "block";
-      }
-      setTimeout(()=>{
-        if(quiz_count === 10){
-          
-        }
-         makeQuestion()
-         document.getElementById('counter').innerHTML = counter + " out of " + quiz_count + " questions are correct"
-         msgSetNone()
-       }, 1000);
-
+    } else {
+      console.log("User Input: ", choices_cls[i].textContent)
+      document.getElementById('incorrect').style.display = "block";
+    }
+    if (quiz_count < 10) {
+      setTimeout(() => {
+        makeQuestion()
+        document.getElementById('counter').innerHTML = counter + " out of " + quiz_count + " questions are correct"
+        msgSetNone()
+      }, 1000);
+    } else {
+      console.log("over 10")
+      location.href = "./result.html"
+    }
   })
 
 }
 
-function msgSetNone(){
+function msgSetNone() {
   document.getElementById('correct').style.display = "none";
   document.getElementById('incorrect').style.display = "none";
 }
